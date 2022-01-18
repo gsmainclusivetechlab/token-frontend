4ee881ef2627d07d30fd568f2b533028230d0bda
Rui Lopes <rui.lopes@altar.io>
2022-01-18 16:12:55 +0000

    refactor(page-interface): layout changed

---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-form-input class="form-group" ref="inputRef" type="text" :placeholder="placeholder" />
+</template>
+
+<script>
+import { useCurrencyInput } from 'vue-currency-input';
+import { watch } from '@vue/composition-api'
+
+export default {
+  name: 'CurrencyInput',
+  props: {
+    value: Number,
+    options: Object,
+    placeholder: String,
+  },
+  setup(props) {
+    const { inputRef, setValue } = useCurrencyInput(props.options);
+
+    watch(
+      () => props.value, // Vue 2: props.value
+      (value) => {
+        setValue(value)
+      }
+    )
+
+    return { inputRef };
+  },
+};
+</script>
